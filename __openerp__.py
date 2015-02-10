4bebc53a91440ded36f44524113bbbd0ae1eb810
ozytwyst <ozydev@julienthomazeau.fr>
2015-02-10 08:53:16 +0100

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    presta_history_report module for OpenERP, Get PrestaShop Connection History
+#    presta_history_report module for OpenERP, Display connections
 #    Copyright (C) 2014 ozytwyst Julien Thomazeau <ozydev@julienthomazeau.fr>
 #
-#    This file is a part of product_brands
+#    This file is a part of presta_history_report
 #
-#    product_brands is free software: you can redistribute it and/or modify
+#    presta_history_report is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    product_brands is distributed in the hope that it will be useful,
+#    presta_history_report is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Affero General Public License for more details.
